d5847f51db9ffaf71597bc7f4119e6be0a999424
Nikita Litvin <n@deltaidea.com>
2016-09-24 00:37:03 +0300

    refactor(ensureUnzipped): add this to replace `install/fromZip`

---
+++ test/ensureUnzipped.spec.js
@@ -0,0 +1,40 @@
+/* eslint-env mocha */
+
+let proxyquire = require('proxyquire')
+
+describe('ensureUnzipped', function () {
+  it('returns passed path when it is actually a folder', function (done) {
+    let ensureUnzipped = proxyquire('../lib/ensureUnzipped', {
+      'fs-extra-promise': {
+        isDirectory: () => Promise.resolve(true)
+      }
+    })
+
+    ensureUnzipped('fake-path').then(resultPath => {
+      resultPath.should.equal('fake-path')
+      done()
+    })
+  })
+
+  it('unzips to a temp directory and returns it', function (done) {
+    let ensureUnzipped = proxyquire('../lib/ensureUnzipped', {
+      'fs-extra-promise': {
+        isDirectory: () => Promise.resolve(false)
+      },
+      'tmp-promise': {
+        dir: () => Promise.resolve({path: 'fake-temp-dir'})
+      },
+      'adm-zip': function (pathToArchive) {
+        pathToArchive.should.equal('fake-path')
+        this.extractAllTo = path => {
+          path.should.equal('fake-temp-dir')
+        }
+      }
+    })
+
+    ensureUnzipped('fake-path').then(result => {
+      result.should.equal('fake-temp-dir')
+      done()
+    })
+  })
+})
