af8acb950c8af7d79e069b446f4f5331ff412830
Nikita Litvin <n@deltaidea.com>
2016-09-23 21:47:33 +0300

    refactor(util/addInstalledPackage): use promises

--- test/util/addInstalledPackage.spec.js
+++ test/util/addInstalledPackage.spec.js
@@ -1,48 +1,45 @@
 /* eslint-env mocha */
 
-let expect = require('chai').expect
 let proxyquire = require('proxyquire')
 
 describe('util.addInstalledPackage', function () {
   it("adds specified module to 'mcpmInstalledPackages'", function (done) {
     let addInstalledPackage = proxyquire('../../lib/util/addInstalledPackage', {
-      './getCurrentProfile' () {
-        return {
-          originalInfo: {
-            mcpmInstalledPackages: {
-              'whatever': '1.2.3'
-            }
+      './getCurrentProfile': () => Promise.resolve({
+        originalInfo: {
+          mcpmInstalledPackages: {
+            'whatever': '1.2.3'
           }
         }
-      },
-      './setCurrentProfile' (newProfile) {
+      }),
+      './setCurrentProfile': (newProfile) => {
         newProfile.mcpmInstalledPackages.should.deep.equal({
           'whatever': '1.2.3',
           'fake-package': '0.1.0'
         })
+        return Promise.resolve(true)
       }
     })
 
-    addInstalledPackage('fake-package', '0.1.0', function (err, result) {
-      expect(err).to.equal(undefined)
-      expect(result).to.equal(true)
+    addInstalledPackage('fake-package', '0.1.0').then(result => {
+      true.should.equal(result)
       done()
     })
   })
 
   it("adds 'mcpmInstalledPackages' field if it's not there yet", function (done) {
     let addInstalledPackage = proxyquire('../../lib/util/addInstalledPackage', {
-      './getCurrentProfile' () {
-        return {originalInfo: {}}
-      },
-      './setCurrentProfile' (newProfile) {
-        '0.1.0'.should.equal(newProfile.mcpmInstalledPackages[ 'fake-package' ])
+      './getCurrentProfile': () => Promise.resolve({
+        originalInfo: {}
+      }),
+      './setCurrentProfile': (newProfile) => {
+        '0.1.0'.should.equal(newProfile.mcpmInstalledPackages['fake-package'])
+        return Promise.resolve(true)
       }
     })
 
-    addInstalledPackage('fake-package', '0.1.0', function (err, result) {
-      expect(err).to.equal(undefined)
-      expect(result).to.equal(true)
+    addInstalledPackage('fake-package', '0.1.0').then(result => {
+      true.should.equal(result)
       done()
     })
   })
