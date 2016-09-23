0baace5ee5b3ee318c61b64c59d5080fcd48d70d
Nikita Litvin <n@deltaidea.com>
2016-09-23 21:19:23 +0300

    refactor(util/getCurrentProfile): use promises

--- test/util/getCurrentProfile.spec.js
+++ test/util/getCurrentProfile.spec.js
@@ -1,87 +1,69 @@
 /* eslint-env mocha */
 
-let expect = require('chai').expect
 let proxyquire = require('proxyquire')
 let path = require('path')
-let fs = require('fs')
 
-// cwd seems to be outside of test/
-let pathToFixtures = path.resolve('test/fixtures')
-let pathToTheFixture = path.join(pathToFixtures, 'launcher_profiles.json')
+let fakeLauncherProfilesOriginal = require('../fixtures/launcher_profiles.json')
+let fakeLauncherProfiles
+let fakeError
 
-let loadFixture = () => JSON.parse(fs.readFileSync(pathToTheFixture, {encoding: 'utf-8'}))
-
-let getCurrentProfile = proxyquire('../../lib/util/getCurrentProfile',
-  {'./getMinecraftPath' () { return pathToFixtures }})
+let getCurrentProfile = proxyquire('../../lib/util/getCurrentProfile', {
+  './getMinecraftPath': () => 'fake-mc-path',
+  'fs': {
+    readFile: (filePath, encoding, callback) => {
+      encoding.should.equal('utf8')
+      filePath.should.equal(`fake-mc-path${path.sep}launcher_profiles.json`)
+      callback(fakeError, JSON.stringify(fakeLauncherProfiles))
+    }
+  }
+})
 
 describe('util.getCurrentProfile', function () {
-  afterEach(function () {
-    // Roll back the changes to make the tests stateless.
-    let fixture = loadFixture()
-    fixture.selectedProfile = '1.8 + Forge + LiteLoader'
-    fixture.profiles[ '1.8 + Forge + LiteLoader' ].lastVersionId = '1.8.0'
-    fs.writeFileSync(pathToTheFixture, JSON.stringify(fixture, null, 2))
+  beforeEach(() => {
+    fakeLauncherProfiles = JSON.parse(JSON.stringify(fakeLauncherProfilesOriginal))
+    fakeError = null
   })
 
   it('returns current profile in originalInfo property', function (done) {
-    let fixture = loadFixture()
-    let actualInfo = fixture.profiles[ '1.8 + Forge + LiteLoader' ]
-
-    getCurrentProfile(function (err, currentProfile) {
-      expect(err).to.equal(undefined)
-      actualInfo.should.deep.equal(currentProfile.originalInfo)
+    getCurrentProfile().then(currentProfile => {
+      fakeLauncherProfiles.profiles['1.8 + Forge + LiteLoader']
+        .should.deep.equal(currentProfile.originalInfo)
       done()
     })
   })
 
   it('returns current Minecraft version in version property', function (done) {
-    getCurrentProfile(function (err, currentProfile) {
-      expect(err).to.equal(undefined)
+    getCurrentProfile().then(currentProfile => {
       '1.8.0'.should.equal(currentProfile.version)
       done()
     })
   })
 
   it("normalizes version to 'x.x.0' when specified as 'x.x'", function (done) {
-    let fixture = loadFixture()
-    fixture.profiles[ '1.8 + Forge + LiteLoader' ].lastVersionId = '1.8'
-    fs.writeFileSync(pathToTheFixture, JSON.stringify(fixture))
-    getCurrentProfile(function (err, currentProfile) {
-      expect(err).to.equal(undefined)
+    fakeLauncherProfiles.profiles['1.8 + Forge + LiteLoader'].lastVersionId = '1.8'
+    getCurrentProfile().then(currentProfile => {
       '1.8.0'.should.equal(currentProfile.version)
       done()
     })
   })
 
   it('returns installed packages in installedPackages property', function (done) {
-    getCurrentProfile(function (err, currentProfile) {
-      expect(err).to.equal(undefined)
-      let packageList = {
+    getCurrentProfile().then(currentProfile => {
+      currentProfile.installedPackages.should.deep.equal({
         fake: '1.2.3',
         package: '2.3.4'
-      }
-      packageList.should.deep.equal(currentProfile.installedPackages)
+      })
       done()
     })
   })
 
-  it('reloads profiles on each call', function (done) {
-    let fixture = loadFixture()
-    let actualInfo = fixture.profiles[ '1.8 + Forge + LiteLoader' ]
-
-    getCurrentProfile(function (err, currentProfile) {
-      expect(err).to.equal(undefined)
-      actualInfo.should.deep.equal(currentProfile.originalInfo)
-
-      fixture.selectedProfile = '1.8'
-      fs.writeFileSync(pathToTheFixture, JSON.stringify(fixture))
-      actualInfo = fixture.profiles[ '1.8' ]
-
-      getCurrentProfile(function (err, currentProfile) {
-        expect(err).to.equal(undefined)
-        actualInfo.should.deep.equal(currentProfile.originalInfo)
-        done()
-      })
+  it('rejects when cannot read launcher_profiles.json', function (done) {
+    fakeError = 'fake-error'
+    getCurrentProfile().then(() => {
+      done(new Error('Should have rejected!'))
+    }, error => {
+      fakeError.should.equal(error)
+      done()
     })
   })
 })
